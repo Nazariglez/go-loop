074d169d03007059be71d27cb566992051e9f61b
Nazarí González <nazari.nz@gmail.com>
2016-10-11 17:17:43 +0100

    gofmt changes

--- loop.go
+++ loop.go
@@ -5,69 +5,69 @@
 package loop
 
 import (
-  "time"
+	"time"
 )
 
 type Loop struct {
-  fps                  float64
-  nanoFps              time.Duration
-  IsRunning            bool
-  ticker               *time.Ticker
-  last, lastTime, time int64
-  Tick                 chan float64
+	fps                  float64
+	nanoFps              time.Duration
+	IsRunning            bool
+	ticker               *time.Ticker
+	last, lastTime, time int64
+	Tick                 chan float64
 }
 
 func NewLoop(fps float64) Loop {
-  loop := Loop{
-    Tick: make(chan float64),
-  }
-  loop.SetFPS(fps)
-  return loop
+	loop := Loop{
+		Tick: make(chan float64),
+	}
+	loop.SetFPS(fps)
+	return loop
 }
 
 func (loop *Loop) SetFPS(fps float64) {
-  restart := false
-  if loop.IsRunning {
-    loop.Stop()
-    restart = true
-  }
+	restart := false
+	if loop.IsRunning {
+		loop.Stop()
+		restart = true
+	}
 
-  loop.fps = fps
-  loop.nanoFps = time.Duration((1/fps)*1e9)*time.Nanosecond
+	loop.fps = fps
+	loop.nanoFps = time.Duration((1/fps)*1e9) * time.Nanosecond
 
-  if restart {
-    loop.Start()
-  }
+	if restart {
+		loop.Start()
+	}
 }
 
 func (loop *Loop) Start() {
-  if loop.IsRunning {
-    return
-  }
+	if loop.IsRunning {
+		return
+	}
 
-  loop.last = time.Now().UnixNano()
-  loop.IsRunning = true
-  loop.ticker = time.NewTicker(loop.nanoFps)
+	loop.last = time.Now().UnixNano()
+	loop.IsRunning = true
+	loop.ticker = time.NewTicker(loop.nanoFps)
 
-  go func() {
-    var now, delta int64
+	go func() {
+		var now, delta int64
 
-    for _ = range loop.ticker.C {
-      now = time.Now().UnixNano()
-      loop.time += (now-loop.last)
-      delta = loop.time -loop.lastTime
-      loop.lastTime = loop.time
-      loop.last = now
-      loop.Tick <- float64(delta)/1e9
-    }
-  }()
+		for _ = range loop.ticker.C {
+			now = time.Now().UnixNano()
+			loop.time += (now - loop.last)
+			delta = loop.time - loop.lastTime
+			loop.lastTime = loop.time
+			loop.last = now
+			loop.Tick <- float64(delta) / 1e9
+		}
+	}()
 }
 
 func (loop *Loop) Stop() {
-  if !loop.IsRunning {
-    return
-  }
+	if !loop.IsRunning {
+		return
+	}
 
-  loop.ticker.Stop()
-  loop.IsRunning = false
+	loop.ticker.Stop()
+	loop.IsRunning = false
 }
